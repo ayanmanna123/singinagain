ee3b3cd26dbea8bc2461833baf54242a4f0a5cb5
Ayan Manna <ayanmanna858@gmail.com>
2025-04-02 23:47:31 +0530

    new

--- src/App.js
+++ src/App.js
@@ -1,25 +1,164 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import { useNavigate } from "react-router-dom";
+import "./styles.css";
 
-function App() {
+const App = () => {
+  const [isLogin, setIsLogin] = useState(true);
+  const [credentials, setCredentials] = useState({
+    name: "",
+    email: "",
+    password: "",
+    cpassword: "",
+  });
+
+  let navigate = useNavigate();
+
+  const onChange = (e) => {
+    setCredentials({ ...credentials, [e.target.name]: e.target.value });
+  };
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    const endpoint = isLogin
+      ? "http://localhost:5000/api/auth/login"
+      : "http://localhost:5000/api/auth/signup";
+
+    const response = await fetch(endpoint, {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(credentials),
+    });
+
+    const json = await response.json();
+    console.log(json);
+
+    if (json.success) {
+      localStorage.setItem("token", json.authtoken);
+      navigate("/");
+    } else {
+      alert("Invalid credentials");
+    }
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className={`container ${isLogin ? "" : "active"}`}>
+      {isLogin ? (
+        <div className="form-box login">
+          <form onSubmit={handleSubmit}>
+            <h1>Login</h1>
+            <div className="input-box">
+              <label htmlFor="email" className="form-label">
+                Email address
+              </label>
+              <input
+                type="email"
+                className="form-control"
+                id="email"
+                name="email"
+                value={credentials.email}
+                onChange={onChange}
+                required
+              />
+            </div>
+            <div className="input-box">
+              <label htmlFor="password" className="form-label">
+                Password
+              </label>
+              <input
+                type="password"
+                className="form-control"
+                id="password"
+                name="password"
+                value={credentials.password}
+                onChange={onChange}
+                required
+              />
+            </div>
+            <button type="submit" className="btn">
+              Login
+            </button>
+          </form>
+        </div>
+      ) : (
+        <div className="form-box register">
+          <form onSubmit={handleSubmit}>
+            <h1>Signup</h1>
+            <div className="input-box">
+              <label htmlFor="name" className="form-label">
+                User Name
+              </label>
+              <input
+                type="text"
+                className="form-control"
+                id="name"
+                name="name"
+                value={credentials.name}
+                onChange={onChange}
+                required
+              />
+            </div>
+            <div className="input-box">
+              <label htmlFor="email" className="form-label">
+                Email address
+              </label>
+              <input
+                type="email"
+                className="form-control"
+                id="email"
+                name="email"
+                value={credentials.email}
+                onChange={onChange}
+                required
+              />
+            </div>
+            <div className="input-box">
+              <label htmlFor="password" className="form-label">
+                Password
+              </label>
+              <input
+                type="password"
+                className="form-control"
+                id="password"
+                name="password"
+                value={credentials.password}
+                onChange={onChange}
+                minLength={5}
+                required
+              />
+            </div>
+
+            <button type="submit" className="btn">
+              Signup
+            </button>
+          </form>
+        </div>
+      )}
+      <div className="toggle-box">
+        {isLogin ? (
+          <div className="toggle-panel toggle-left">
+            <h1>Hello, Welcome!</h1>
+            <p>Don't have an account?</p>
+            <button
+              className="btn register-btn"
+              onClick={() => setIsLogin(false)}
+            >
+              Register
+            </button>
+          </div>
+        ) : (
+          <div className="toggle-panel toggle-right">
+            <h1>Welcome Back!</h1>
+            <p>Already have an account?</p>
+            <button className="btn login-btn" onClick={() => setIsLogin(true)}>
+              Login
+            </button>
+          </div>
+        )}
+      </div>
     </div>
   );
-}
+};
 
 export default App;
